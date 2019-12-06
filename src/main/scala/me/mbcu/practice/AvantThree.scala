f6089bed01b423d0436410fd9db91bc04ead7103
inmyth <ad3a5a3adc570d224e6509a39bee52cc0d36b55b>
2019-12-07 01:18:36 +0900

    Avant two

--- src/main/scala/me/mbcu/practice/AvantThree.scala
+++ src/main/scala/me/mbcu/practice/AvantThree.scala
@@ -2,6 +2,33 @@ package me.mbcu.practice
 
 object AvantThree extends App{
 
+  /*
+    Fix this method to find occurences in an array
+
+    def solution(M: Int, A: Array[Int]): Int = {
+      var N: Int = A.length;
+      var count: Array[Int] = Array.fill(M + 1)(0);
+      var maxOccurence: Int = 1;
+      var index: Int = -1;
+      var i: Int = 0;
+      while (i < N) {
+        if (A(i) <= M) { // 1
+          var tmp: Int = count(A(i)) + 1; //3
+          if (tmp > maxOccurence) {
+            maxOccurence = tmp;
+            index = i;
+          }
+          count(A(i)) = tmp; //4
+        }
+        else {
+  //        count(A(i)) = 1; //2
+        }
+        i = i + 1;
+      }
+      return A(index);
+    }
+ */
+
   val res1 = solution0(2, Array(1,2,3,3,1,3,1))
   println(res1)
 //  val res2 = solution0(7, Array(1,4,4,3,1,3,1,4, 1, 5, 6, 6, 7,8,6,6, 7,6))
@@ -54,28 +81,4 @@ object AvantThree extends App{
 
 }
 
-/*
 
-
-    def solution(M: Int, A: Array[Int]): Int = {
-        var N: Int = A.length;
-        var count: Array[Int] = Array.fill(M + 1)(0);
-        var maxOccurence: Int = 1;
-        var index: Int = -1;
-        var i: Int = 0;
-        while (i < N) {
-            if (count(A(i)) > 0) {
-                var tmp: Int = count(A(i));
-                if (tmp > maxOccurence) {
-                    maxOccurence = tmp;
-                    index = i;
-                }
-                count(A(i)) = tmp + 1;
-            } else {
-                count(A(i)) = 1;
-            }
-            i = i + 1;
-        }
-        return A(index);
-    }
- */
